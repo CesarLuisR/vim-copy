f2cb8c68a7b3bd7d8a3e2043581fcf78ced18d9a
Cesar Luis Rijo Cedano <cesarluisrc@gmail.com>
2024-10-11 22:21:05 -0400

    Dique funciona el insert, delete, replace y estan los cimientos de la stack

--- src/piece-table.c
+++ src/piece-table.c
@@ -2,6 +2,55 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct stack_node_t {
+    struct stack_node_t* next;
+    const char* value;
+} stack_node_t;
+
+typedef struct {
+    stack_node_t* head;
+} stack_t;
+
+stack_t* stack_create() {
+    stack_t* stack = malloc(sizeof(stack_t));
+    stack->head = NULL;
+    return stack;
+}
+
+void stack_push(stack_t* stack, const char* value) {
+    stack_node_t* node = malloc(sizeof(stack_node_t));
+    node->next = NULL;
+    node->value = strdup(value);
+
+    if (stack->head == NULL) {
+        stack->head = node;
+    } else {
+        node->next = stack->head;
+        stack->head = node;
+    }
+}
+
+void stack_pop(stack_t* stack) {
+    if (stack->head == NULL) return;
+
+    if (stack->head->next == NULL) {
+        stack->head = NULL;
+    } else {
+        stack_node_t* current = stack->head;
+        stack->head = stack->head->next;
+        free(current->value);
+        free(current);
+    }
+}
+
+void stack_dump(stack_t* stack) {
+    if (stack->head != NULL) {
+        printf("El valor es: %s\n", stack->head->value);
+    } else {
+        printf("Stack vacia\n");
+    }
+};
+
 typedef struct {
     char source[9];
     int index;
@@ -63,11 +112,9 @@ void display_text(piece_table_t* pt) {
             ? pt->original_buffer
             : pt->append_buffer;
         
-        printf("PIECE: ");
         for (int i = 0; i < current->data->length; i++) 
             putchar(buffer[current->data->index + i]);
-        putchar('\n');
-        
+
         current = current->next;
     }
 
@@ -78,10 +125,10 @@ void insert_text(piece_table_t* pt, const char* new_text, int position) {
     int new_text_len = strlen(new_text);
 
     if (pt->append_buffer == NULL) {
-        pt->append_buffer = malloc(new_text_len);
+        pt->append_buffer = malloc(new_text_len + 1);
         strcpy(pt->append_buffer, "");
     } else {
-        pt->append_buffer = realloc(pt->append_buffer, sizeof(pt->append_buffer) + new_text_len);
+        pt->append_buffer = realloc(pt->append_buffer, strlen(pt->append_buffer) + new_text_len + 1);
     }
 
     strcat(pt->append_buffer, new_text);
@@ -97,9 +144,6 @@ void insert_text(piece_table_t* pt, const char* new_text, int position) {
 
             // Cutting the first half 
             current->data->length = split_text;
-            if (current->data->length == 0) {
-                current->prev = after_piece;
-            }
 
             after_piece->next = current->next;
             if (current->next != NULL) {
@@ -107,6 +151,21 @@ void insert_text(piece_table_t* pt, const char* new_text, int position) {
             }
 
             piece_t* new_piece = create_piece("append", strlen(pt->append_buffer) - new_text_len, new_text_len);
+
+            if (current->data->length == 0) {
+                if (current->prev)
+                    current->prev->next = new_piece;
+
+                if (current == pt->sequence->head)
+                    pt->sequence->head = new_piece;
+                
+                new_piece->next = after_piece;
+                after_piece->prev = new_piece;
+                if (current->next == pt->sequence->tail) 
+                    pt->sequence->tail = after_piece;
+
+                return;
+            }
 
             current->next = new_piece;
             new_piece->prev = current;
@@ -140,18 +199,21 @@ void delete_text(piece_table_t* pt, int index, int length) {
 
             first_span->next = last_span;
             last_span->prev = first_span;
-            last_span->next = current->next;
 
             if (current->prev != NULL) current->prev->next = first_span;
             else pt->sequence->head = first_span;
             
-            if (current->next != NULL) current->next->prev = last_span;
-            else pt->sequence->tail = last_span;
-            
+            if (current->next != NULL) {
+                last_span->next = current->next;
+                current->next->prev = last_span;
+            } else pt->sequence->tail = last_span;
+
+            free(current->data);
+            free(current);
             return;
         }
 
-        if (index >= piece_start && index <= piece_end) {
+        if (index >= piece_start && index < piece_end) {
             first_piece = create_piece(current->data->source, current->data->index, index - piece_start);
 
             if (current->prev) {
@@ -166,15 +228,16 @@ void delete_text(piece_table_t* pt, int index, int length) {
             last_piece = create_piece(current->data->source, current->data->index + (index + length - piece_start), piece_end - (index + length));
 
             if (current->next) {
-                last_piece->next = current->next;
                 current->next->prev = last_piece;
+                last_piece->next = current->next;
             } else pt->sequence->tail = last_piece;
 
             last_piece->prev = NULL;
         }
 
-        if (index + length >= piece_start && index <= piece_end) {
+        if (index + length > piece_start && index < piece_end) {
             piece_t* to_delete = current;
+
             free(to_delete->data);
             free(to_delete);
         } 
@@ -189,53 +252,7 @@ void delete_text(piece_table_t* pt, int index, int length) {
     }
 }
 
-// Replace text
-void replace_text(piece_table_t* pt, int index, int length, const char* new_text) {
-    int new_text_len = strlen(new_text);
-
-    if (pt->append_buffer == NULL) {
-        pt->append_buffer = malloc(new_text_len);
-        strcpy(pt->append_buffer, "");
-    } else {
-        pt->append_buffer = realloc(pt->append_buffer, sizeof(pt->append_buffer) + new_text_len);
-    }
-
-    strcat(pt->append_buffer, new_text);
-
-    piece_t* current = pt->sequence->head;
-
-    int cur_pos = 0;
-    while (current != NULL) {
-        int piece_start = cur_pos;
-        int piece_end = cur_pos + current->data->length;
-
-        if (index >= piece_start && index + length <= piece_end) {
-            piece_t* first_span = create_piece(current->data->source, current->data->index, index - piece_start);
-            piece_t* last_span = create_piece(current->data->source, current->data->index + (index + length - piece_start), piece_end - (index + length));
-            piece_t* new_piece = create_piece("append", strlen(pt->append_buffer) - new_text_len, new_text_len);
-            // insert_text(pt, new_text, index);
-
-            first_span->next = new_piece;
-            new_piece->next = last_span;
-            last_span->prev = new_piece;
-            new_piece->prev = first_span;
-            last_span->next = current->next;
-
-            if (current->prev != NULL) current->prev->next = first_span;
-            else pt->sequence->head = first_span;
-            
-            if (current->next != NULL) current->next->prev = last_span;
-            else pt->sequence->tail = last_span;
-
-            return;
-        }
-
-        cur_pos += current->data->length;
-        current = current->next;
-    }
-}
-
-void replace(piece_table_t* pt, int index, const char* new_text) {
+void replace_text(piece_table_t* pt, int index, const char* new_text) {
     delete_text(pt, index, strlen(new_text));
     insert_text(pt, new_text, index);
 }
@@ -245,14 +262,24 @@ void replace(piece_table_t* pt, int index, const char* new_text) {
 int main(void) {
     const char* original = "Hola a todos";
     piece_table_t* pt = pt_create(original);
+    stack_t* stack = stack_create();
+    // stack_push(stack, "Hola");
+    // stack_push(stack, "Despues de hola");
+    // stack_push(stack, "Ultimo valor");
+    // stack_pop(stack);
+    // stack_pop(stack);
+    // stack_pop(stack);
+    // stack_dump(stack);
 
-    insert_text(pt, " Joel y a", 6);
-    insert_text(pt, " 7D", 21);
-    delete_text(pt, 7, 2);
-    delete_text(pt, 0, 2);
-    insert_text(pt, "CA", 0);
+    // insert_text(pt, " Joel y a", 6);
+    // insert_text(pt, "Al inicio: ", 0);
 
-    display_text(pt);
+    // display_text(pt);
+
+    // replace_text(pt, 2, "TODO ESTO VA PARA xdxdxdxdxdODOSaa");
+    // replace_text(pt, 0, "CO");
+
+    // display_text(pt);
 
     return 0;
 }
